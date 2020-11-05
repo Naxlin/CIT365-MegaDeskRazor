ef9b20909ac462a091b11ff16c06863caf656d72
Suyeon Yang <yan17002@byui.edu>
2020-11-05 16:33:15 -0700

    Scaffolding failed

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MegaDeskRazor.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MegaDeskRazor
 {
@@ -24,6 +26,9 @@ namespace MegaDeskRazor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            
+            services.AddDbContext<MegaDeskContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("DeskContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
